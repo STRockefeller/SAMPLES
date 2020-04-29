54986995c121a55d8560f762d998892c01b4f559
Rockefeller <kenny71223@gmail.com>
2020-04-29 17:06:22 +0800

    20200429update

---
+++ 各類程式範例/C#/等待的寫法.cs
@@ -0,0 +1,11 @@
+using System.Threading
+
+//SpinWait.SpinUntil(() => { return 'PS1''PS2'; }, 'PS3');
+//PS1 這裡回傳true會跳離；若回傳false則繼續執行
+//PS2 這裡的方法會被不斷執行至回傳true或設定時間到達
+//PS3 設定時間(ms)，若時間到仍未得到true回傳值則跳脫
+SpinWait.SpinUntil(() => { return false; }, 5000);
+//範例 總之就是等5秒
+
+//另一種寫法(較不推薦)
+Thread.Sleep(5000);
--- 各類程式範例/Web/typeScript/readMe.txt
+++ 各類程式範例/Web/typeScript/readMe.txt
@@ -1,5 +1,5 @@
 安裝node.js
 npm安裝typescript
-
+ npm install -g typescript
 若要使用jQuery
 npm install @types/jquery --save-dev
